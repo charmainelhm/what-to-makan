b28ad3ccfd193b260940c5307f40e14f18f6e199
Charmaine Lee <charmainelhm@gmail.com>
2022-11-01 12:59:29 +0800

    Refactor roulette component

--- src/components/Roulette.js
+++ src/components/Roulette.js
@@ -1,69 +1,51 @@
 import { useState, useEffect } from "react";
 
+const wheelOptions = [
+  { name: "Korean", number: "one" },
+  { name: "Western", number: "two" },
+  { name: "Chinese", number: "three" },
+  { name: "Malay", number: "four" },
+  { name: "Indian", number: "five" },
+  { name: "Japanese", number: "six" },
+];
+
 export default function Roulette() {
-  const [rotation, setRotation] = useState("0");
+  const [rotation, setRotation] = useState("0"); // prevent value from updating when other states updates reactDOM
+
+  const wheelSectionArr = wheelOptions.map((option, ind) => {
+    return (
+      <div
+        className={`section-${option.number}`}
+        key={ind}
+        contentEditable="true"
+        spellCheck="false"
+      >
+        {option.name}
+      </div>
+    );
+  });
 
   const spinTheWheel = () => {
     const randomValue = Math.round(Math.random() * 720) + 360;
     setRotation(String(randomValue + rotation * 1));
+    wheelSectionArr.forEach((option) =>
+      console.log(option.getBoundingClientRect())
+    );
   };
 
   return (
     <>
       <p>Can't decide what to eat? Let the wheel decide for you!</p>
-      <button onClick={spinTheWheel}>Spin the Wheel</button>
       <div className="wheel-wrapper">
         <div className="wheel" style={{ transform: `rotate(${rotation}deg)` }}>
-          <div
-            className="section-one"
-            contentEditable="true"
-            spellCheck="false"
-          >
-            Korean
-          </div>
-          <div
-            className="section-two"
-            contentEditable="true"
-            spellCheck="false"
-          >
-            Western
-          </div>
-          <div
-            className="section-three"
-            contentEditable="true"
-            spellCheck="false"
-          >
-            Chinese
-          </div>
-          <div
-            className="section-four"
-            contentEditable="true"
-            spellCheck="false"
-          >
-            Malay
-          </div>
-          <div
-            className="section-five"
-            contentEditable="true"
-            spellCheck="false"
-          >
-            Indian
-          </div>
-          <div
-            className="section-six"
-            contentEditable="true"
-            spellCheck="false"
-          >
-            Japanese
-          </div>
-          <span
-            className="wheel-pin"
-            contentEditable="true"
-            spellCheck="false"
-          ></span>
+          {wheelSectionArr}
+          <span className="wheel-pin"></span>
         </div>
         <div className="wheel-stopper"></div>
       </div>
+      <button className="mt-6" onClick={spinTheWheel}>
+        Spin the Wheel
+      </button>
     </>
   );
 }
