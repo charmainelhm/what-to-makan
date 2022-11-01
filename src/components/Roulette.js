ddea424521b4d1781ebcc8f8e187229ef00c4f5a
Charmaine Lee <charmainelhm@gmail.com>
2022-11-01 12:29:42 +0800

    Allow wheel to spin when button is clicked

--- src/components/Roulette.js
+++ src/components/Roulette.js
@@ -1,17 +1,66 @@
+import { useState, useEffect } from "react";
+
 export default function Roulette() {
+  const [rotation, setRotation] = useState("0");
+
+  const spinTheWheel = () => {
+    const randomValue = Math.round(Math.random() * 720) + 360;
+    setRotation(String(randomValue + rotation * 1));
+  };
+
   return (
     <>
       <p>Can't decide what to eat? Let the wheel decide for you!</p>
-      <button>Spin the Wheel</button>
+      <button onClick={spinTheWheel}>Spin the Wheel</button>
       <div className="wheel-wrapper">
-        <div className="wheel">
-          <div className="section-one">Korean</div>
-          <div className="section-two">Western</div>
-          <div className="section-three">Chinese</div>
-          <div className="section-four">Malay</div>
-          <div className="section-five">Indian</div>
-          <div className="section-six">Japanese</div>
-          <span className="wheel-pin"></span>
+        <div className="wheel" style={{ transform: `rotate(${rotation}deg)` }}>
+          <div
+            className="section-one"
+            contentEditable="true"
+            spellCheck="false"
+          >
+            Korean
+          </div>
+          <div
+            className="section-two"
+            contentEditable="true"
+            spellCheck="false"
+          >
+            Western
+          </div>
+          <div
+            className="section-three"
+            contentEditable="true"
+            spellCheck="false"
+          >
+            Chinese
+          </div>
+          <div
+            className="section-four"
+            contentEditable="true"
+            spellCheck="false"
+          >
+            Malay
+          </div>
+          <div
+            className="section-five"
+            contentEditable="true"
+            spellCheck="false"
+          >
+            Indian
+          </div>
+          <div
+            className="section-six"
+            contentEditable="true"
+            spellCheck="false"
+          >
+            Japanese
+          </div>
+          <span
+            className="wheel-pin"
+            contentEditable="true"
+            spellCheck="false"
+          ></span>
         </div>
         <div className="wheel-stopper"></div>
       </div>
