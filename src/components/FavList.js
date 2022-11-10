54ecbd2d1c16537991dcaef84c396b07801672b8
Charmaine Lee <charmainelhm@gmail.com>
2022-11-10 11:49:40 +0800

    Change message for favlist component

--- src/components/FavList.js
+++ src/components/FavList.js
@@ -18,10 +18,12 @@ export default function FavList() {
   return (
     <>
       <h3>Eat These!</h3>
+      <p style={{ display: foodArr.length === 0 ? "block" : "none" }}>
+        Places you have liked will be found here, start adding restaurants to
+        your collection now!
+      </p>
       <div className="grid grid-cols-auto-fill gap-5 py-8 w-11/12 mx-auto">
-        {foodArr.length === 0
-          ? "Places you have liked will be found here, start adding restaurants to your collection now!"
-          : foodArr}
+        {foodArr}
       </div>
     </>
   );
